cc31defe8b2902cb12f1fc56381cbd061bcea0f7
Vamshi Muthyapu <mvamshi14@gmail.com>
2020-04-04 18:59:54 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vamshi/go-crud
+
+go 1.14
+
+require github.com/golang/example v0.0.0-20170904185048-46695d81d1fa
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+)
+
+func main(){
+	fmt.Println("Hello World")
+	fmt.Printf("reverse of Vamshi is %s\n",stringutil.Reverse("Vamshi"))
+
+}
