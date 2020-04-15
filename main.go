5130dc73350a70a5d13e394d437525a5a276108d
Vamshi Muthyapu <mvamshi14@gmail.com>
2020-04-14 20:10:26 -0400

    dockerized this simple app

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/golang/example/stringutil"
 )
 
 func main(){
 	fmt.Println("Hello World")
 	fmt.Printf("reverse of Vamshi is %s\n",stringutil.Reverse("Vamshi"))
+
 
 }
